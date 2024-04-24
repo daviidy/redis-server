04e73f58a6b9791182d5ffe130f7e32b40932a73
David Yao <davidyao@Davids-MacBook-Air.local>
2024-04-23 20:21:31 -0700

    stage 3: Handle concurrent clients

--- app/server.rb
+++ app/server.rb
@@ -9,8 +9,8 @@ class YourRedisServer
   def start
     loop do
       fds_to_watch = [@server, *@clients]
-      ready_fds = IO.select(fds_to_watch)
-      ready_fds.each do |fd|
+      ready_to_read, _, _ = IO.select(fds_to_watch)
+      ready_to_read.each do |fd|
         case fd
         when @server
           accept_client
